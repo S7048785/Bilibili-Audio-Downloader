51a984c27b32da9c0dde6a202b39f7bc49675b62
S7048785 <2889157408@qq.com>
2026-01-23 22:27:33 +0800

    feat(theme): 支持主题模式的持久化与初始化
    - 在初始化时从本地存储读取主题模式偏好
    - 根据读取结果设置主题为暗色或亮色
    - 切换主题时同步更新本地存储中的模式值
    - 添加主题模式切换时的提示文字显示
    - 使用 SharedPreferences 实现主题模式持久化存储功能

--- lib/theme/theme_controller.dart
+++ lib/theme/theme_controller.dart
@@ -1,6 +1,8 @@
 import 'package:bilibili_audio_downloader/theme/theme.dart';
+import 'package:bilibili_audio_downloader/utils/ToastUtil.dart';
 import 'package:forui/forui.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeController extends GetxController {
   final FThemeData lightTheme = slateLight; // 实例化一次
@@ -10,10 +12,29 @@ class ThemeController extends GetxController {
 
   FThemeData get theme => _theme.value;
 
+  @override
+  void onInit() async {
+    super.onInit();
+    final prefs = await SharedPreferences.getInstance();
+    var isDark = prefs.getBool("isDarkMode");
+
+    if (isDark == null) {
+      setThemeMode(false);
+      isDark = false;
+    }
+    ToastUtil.showText(isDark ? "Dark Mode" : "Light Mode");
+    _theme.value = isDark ? darkTheme : lightTheme;
+  }
+
   void toggleTheme() {
     final isLight = identical(_theme.value, lightTheme); // 引用比较更稳妥
-    print('isLight: $isLight');
     _theme.value = isLight ? darkTheme : lightTheme;
+    setThemeMode(isLight);
+  }
+
+  Future<void> setThemeMode(bool isDark) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isDarkMode', isDark);
   }
 
   bool get isDark => identical(_theme.value, darkTheme);
